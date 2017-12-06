7627eb554395882b679281140b30635cd74a4527
Marcus <marcusmallum@gmail.com>
2017-12-05 22:02:02 -0600

    Moved code to ssd1306.c and ssd1306.h files

---
+++ OLED/OLED/Inc/ssd1306.h
@@ -0,0 +1,19 @@
+/*
+ * ssd1306.h
+ *
+ *  Created on: Dec 5, 2017
+ *      Author: Marcus
+ */
+
+#ifndef SSD1306_H_
+#define SSD1306_H_
+/*Public Function Prototypes*/
+void ssd1306Init();
+void ssd1306WriteCommand(uint8_t command);
+void clearScreen();
+void updateScreen();
+void ssd1306_DrawPixel(uint16_t x, uint16_t y, uint8_t color);
+void ssd1306_WriteData(uint8_t* data, uint16_t count);
+char ssd1306_WriteChar(char ch, uint8_t color);
+char ssd1306_WriteString(char* str, uint8_t color);
+#endif /* SSD1306_H_ */
