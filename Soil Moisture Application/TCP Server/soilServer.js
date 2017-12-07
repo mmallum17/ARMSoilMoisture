4cb9080013b7ba0ade49f08d1d1c2d59997fa136
Marcus <marcusmallum@gmail.com>
2017-12-06 21:40:06 -0600

    Inserted sensor data into database success

--- Soil Moisture Application/TCP Server/soilServer.js
+++ Soil Moisture Application/TCP Server/soilServer.js
@@ -1,14 +1,7 @@
 const net = require('net');
-/*const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 
 let url = "mongodb://mmallum:soccer17@ds033196.mlab.com:33196/soil_moisture";
-
-MongoClient.connect(url, function(err, db){
-   if(err){
-       throw err;
-   }
-   console.log("Database created")
-});*/
 
 
 
@@ -22,6 +15,30 @@ c.on('end', () => {
 });
 c.on('data', function (chunk) {
     console.log(chunk.toString());
+    let tokens = chunk.toString().split(",");
+    let moisture = parseFloat(tokens[0]);
+    let temperature = parseFloat(tokens[1]);
+    MongoClient.connect(url, function(err, db){
+        if(err){
+            throw err;
+        }
+        console.log("Database connected");
+        let myobj = { date: new Date(), sensor: 0, value: moisture};
+        db.collection("Moisture").insertOne(myobj, function(err, res) {
+            if(err){
+                throw err;
+            }
+            console.log("Moisture Inserted");
+        });
+        myobj = { date: new Date(), sensor: 0, value: temperature};
+        db.collection("Temperature").insertOne(myobj, function(err, res) {
+            if(err){
+                throw err;
+            }
+            console.log("Temperature Inserted");
+        });
+        db.close();
+    });
 });
 });
 server.on('error', (err) => {
